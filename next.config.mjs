08c7bdc94ca32e86a79f8eb04752d8d898e934fa
Redari-Es <905422030@qq.com>
2024-06-26 18:48:49 +0800

    travel_app_ui_ux

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // 导出设置
+    output: 'export',
+    images:{unoptimized:true}
+};
 
 export default nextConfig;
